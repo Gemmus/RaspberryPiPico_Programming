0668f33964213472f06815af70be81fd27b23930
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-04-28 14:17:26 +0300

    Add files via upload

---
+++ week3_task.py
@@ -0,0 +1,7 @@
+from piotimer import Piotimer as Timer # hardware
+# from machine import Timer #software
+from ssd1306 import SSD1306_I2C
+from machine import Pin, ADC, I2C, PWM
+from fifo import Fifo
+from led import Led
+import utime
